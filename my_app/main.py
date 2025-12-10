b6f5d7cb565e48539ff15c140c1dbba7f2108d27
kundan rai <987kundanrai@gmail.com>
2025-12-10 22:58:36 +0530

    Changes made to template files

--- my_app/main.py
+++ my_app/main.py
@@ -1,10 +1,7 @@
-import os
-
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from my_app.routers import web 
-
+from my_app.routers import web
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="my_app/static"), name="static")
--- my_app/routers/web.py
+++ my_app/routers/web.py
@@ -1,6 +1,16 @@
+from typing import Union
+
 from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    in_offer: Union[bool, None] = None
+
 
 # We use APIRouter instead of FastAPI() here
 router = APIRouter()
@@ -9,6 +19,17 @@ router = APIRouter()
 # Note: We use "app/templates" because the code is run from the root folder
 templates = Jinja2Templates(directory="my_app/templates")
 
+
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@router.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"name": item.name, "item_id": item_id}
